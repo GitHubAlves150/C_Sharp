078754ac88bfb31639f1c4b72d9ea2a657158a0d
Lucas Lorenço Alves <senoratec.io@gmail.com>
2024-06-17 10:01:28 -0300

    Full CommitFull Comit

--- AppMVVM/Models/Veiculos.cs
+++ AppMVVM/Models/Veiculos.cs
@@ -9,7 +9,7 @@ namespace AppMVVM.Models
     public class Veiculos
     {
 
-        public int Id { get; set; }
+        public int ID { get; set; }
 
         public string Nome { get; set; }
 
@@ -22,9 +22,12 @@ namespace AppMVVM.Models
 
             List<Veiculos> listaVeiculos = new List<Veiculos>()
             {
-                new Veiculos {Id=1, Nome="Ford Fiesta", Modelo="1.0 MPI PERSONALITÉ SEDAN 4P", Ano=2005},
-                new Veiculos {Id=2, Nome="Honda crv", Modelo="2.0LX 4x2 16V", Ano=2018},
-                new Veiculos {Id=3, Nome="Ladn Rover", Modelo="3.0 SE 4X4", Ano=2021},
+                new Veiculos {ID=1, Nome="Ford Fiesta", Modelo="1.0 MPI PERSONALITÉ SEDAN 4P", Ano=2005},
+                new Veiculos {ID=2, Nome="Honda crv", Modelo="2.0LX 4x2 16V", Ano=2018},
+                new Veiculos {ID=3, Nome="Ladn Rover", Modelo="9.0 SERA 1X1", Ano=2021},
+                 new Veiculos {ID=1, Nome="PALIO BRANCO", Modelo="0.0 MPI PERSONALITÉ SEDAN 4P", Ano=2005},
+                new Veiculos {ID=2, Nome="HONDA MONARK", Modelo="2.0 PORTUGUES 4x0 1V", Ano=1920},
+                new Veiculos {ID=3, Nome="CELTA ", Modelo="3.0 SE 4X4", Ano=2003}
 
             };
             return listaVeiculos;
