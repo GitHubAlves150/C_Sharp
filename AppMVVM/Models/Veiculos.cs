cadddc6b30d26fa1583118a0ab4243ce1e885ec5
Lucas Lorenço Alves <senoratec.io@gmail.com>
2024-06-17 08:35:57 -0300

    Full Comit

---
+++ AppMVVM/Models/Veiculos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMVVM.Models
+{
+    public class Veiculos
+    {
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Modelo { get; set; }
+
+        public short Ano { get; set; }
+
+        public List<Veiculos> ObterVeivulos()
+        {
+
+            List<Veiculos> listaVeiculos = new List<Veiculos>()
+            {
+                new Veiculos {Id=1, Nome="Ford Fiesta", Modelo="1.0 MPI PERSONALITÉ SEDAN 4P", Ano=2005},
+                new Veiculos {Id=2, Nome="Honda crv", Modelo="2.0LX 4x2 16V", Ano=2018},
+                new Veiculos {Id=3, Nome="Ladn Rover", Modelo="3.0 SE 4X4", Ano=2021},
+
+            };
+            return listaVeiculos;
+        }
+
+
+    }
+}
---
+++ AppMVVM/Views/VeiculoBuscar.xaml.cs
@@ -0,0 +1,35 @@
+﻿using AppMVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppMVVM.Views
+{
+    /// <summary>
+    /// Interação lógica para VeiculoBuscar.xam
+    /// </summary>
+    public partial class VeiculoBuscar : Page
+    {
+        public VeiculoBuscar()
+        {
+            InitializeComponent();
+            DataContext = new VeiculosViewModel();
+        }
+
+        private void txtMarca_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
